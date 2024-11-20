7082d5995e524073b77bfef73a45c667c28d23db
amretasrerengarajan <rengarajanthiruven.a@northeastern.edu>
2024-11-20 16:17:19 -0500

    created health checks fo db

---
+++ src/main/java/com/assignmentManagementSys/rest_demo/controller/HealthCheckController.java
@@ -0,0 +1,51 @@
+package com.assignmentManagementSys.rest_demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@RestController
+@RequestMapping("/healthz")
+public class HealthCheckController implements ErrorController {
+
+    @Autowired
+    private DataSource dataSource; // To access DB connection
+
+    @GetMapping
+    public ResponseEntity<String> healthCheck() {
+        try (Connection connection = dataSource.getConnection()) {
+            if (connection != null && !connection.isClosed()) {
+                return ResponseEntity.ok("Connection to DB is healthy");
+            } else {
+                return ResponseEntity.status(503).body("Database connection failed!");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return ResponseEntity.status(503).body("Database connection failed!");
+        }
+    }
+
+    private ResponseEntity<String> invalidMethods() {
+        return ResponseEntity.status(405).body("Method not allowed!");
+    }
+
+    @PostMapping
+    public ResponseEntity<String> healthCheckPost() {
+        return invalidMethods();
+    }
+
+    @PutMapping
+    public ResponseEntity<String> healthCheckPut() {
+        return invalidMethods();
+    }
+
+    @DeleteMapping
+    public ResponseEntity<String> healthCheckDelete() {
+        return invalidMethods();
+    }
+}
